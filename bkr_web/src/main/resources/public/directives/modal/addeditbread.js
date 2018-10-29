3bb43e4ee7f97daab690cfdb205b6d3a1be44f81
denny <denny.serrano87@gmail.com>
2018-10-29 22:42:42 +0800

    changes

---
+++ bkr_web/src/main/resources/public/directives/modal/addeditbread.js
@@ -0,0 +1,16 @@
+app.directive("addEditBread",function(){
+
+
+
+return {
+
+
+	link:function($scope){
+		
+		
+	},
+    restrict: 'E',
+    templateUrl: '../../../directives/modal/addeditbread.html'
+  };
+
+});

19ba23de9d7f31e55d35b36c4a6001605c8668d4
denny <denny.serrano87@gmail.com>
2018-11-11 00:09:38 +0800

    changes

--- bkr_web/src/main/resources/public/secure/dailysales/directive/detailtable.js
+++ bkr_web/src/main/resources/public/secure/dailysales/directive/detailtable.js
@@ -18,6 +18,33 @@ app.directive("detailTable",function(){
 				return sum;
 			}
 			
+			$(document).ready(function(){
+
+				
+				$(".number").on("blur",function(e){
+					$(this).val(function(index, value) {
+//					    return value
+//					    .replace(/\D/g, "")
+//					    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+					    
+						return ReplaceNumberWithCommas(value);
+						
+					  });
+					
+					function ReplaceNumberWithCommas(yourNumber) {
+					    //Seperates the components of the number
+					    var components = yourNumber.toString().split(".");
+					    //Comma-fies the first part
+					    components [0] = components [0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+					    //Combines the two sections
+					    return components.join(".");
+					}
+					
+				});
+				
+				
+			});
+			
 			
 		},
 		restrict:"E",

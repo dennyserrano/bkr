f417cfdbc6a1408cda52d6d7c1f4ce7bfa7bc5d4
denny <denny.serrano87@gmail.com>
2018-11-22 23:01:04 +0800

    security

--- bkr_web/src/main/java/org/bkr/conf/Conf.java
+++ bkr_web/src/main/java/org/bkr/conf/Conf.java
@@ -1,16 +1,26 @@
 package org.bkr.conf;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAutoConfiguration
 @EnableJpaRepositories("org.bkr.services.repo")
 @EntityScan("org.bkr.models")
 @ComponentScan({"org.bkr.services.service","org.bkr.web"})
+@Configuration
 public class Conf {
-
+	
 }
--- bkr_web/src/main/java/org/bkr/init/Runner.java
+++ bkr_web/src/main/java/org/bkr/init/Runner.java
@@ -1,7 +1,7 @@
 package org.bkr.init;
 
 import org.bkr.conf.Conf;
-//import org.bkr.conf.SecurityConf;
+import org.bkr.conf.SecurityConf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +11,7 @@ public class Runner {
 	public static void main(String[] args) {
 		SpringApplication.run(new Class[] {
 				Conf.class,
-//				SecurityConf.class
+				SecurityConf.class
 				},args);
 	}
 }
